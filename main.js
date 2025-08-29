9e258ecf163c7cb33310366a3d65cccac2c48184
Amber Li (niconotpig) <niconotpig@gmail.com>
2025-08-29 10:07:31 -0400

    day 5 #2

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 function textChange() {
-
-    document.getElementById("bigbox").innerHTML = 'Will the defense choose a counterargument?';
-    document.getElementById("aboutme").style.display = "none";
-}
-function closeButton(){
-
+    const bigbox=document.getElementById("bigbox")
+   
+    bigbox.innerHTML = 'Will the defense choose a counterargument?';
+                 
+    // textbox.addEventListener ('click', function(){
+    //     let evidence = document.getElementById("wamajama");
+    //     if (evidence.style.display ==="block");
+    //          textbox.onclick = evidence.style.display = "none";
+    // })
 }
 function showButton() {
     document.getElementById("buttontab").style.display = "block";
@@ -17,8 +20,10 @@ function showButton() {
 //     textbox.innerHTML = info;
 // }
 
-function displayInfo(evt, infoType){
-    
-    document.getElementById(infoType).style.display = "block";
+function displayInfo(infoType){
+    let info = document.getElementById(infoType);
+    const textbox=document.getElementById("textbox");
     document.getElementById("buttontab").style.display = "none";
+    info.style.display = "block";
+    // window.onclick = info.style.display = "none";
 }
